fd13820137fcd47702fe8cf82b9efce4eb237ad2
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2018-06-02 09:33:49 +0200

    WIP

---
+++ app.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+
+	nats "github.com/nats-io/go-nats"
+)
+
+func main() {
+	nc, err := nats.Connect("nats://localhost:4222")
+	if err != nil {
+		fmt.Println(err)
+	}
+	nc.Subscribe(">", func(m *nats.Msg) {
+		fmt.Printf("Received a message: %s on %s\n", len(m.Data), m.Subject)
+	})
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	nc.Publish("foo.bar.baz", []byte("Hello World"))
+
+	wg.Wait()
+}
--- trigger/nats/trigger.go
+++ trigger/nats/trigger.go
@@ -1,27 +1,30 @@
 package nats
 
 import (
-	"github.com/TIBCOSoftware/flogo-lib/core/action"
-	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
-	"github.com/nats-io/go-nats"
-	"fmt"
 	"context"
 	"encoding/json"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/action"
+	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+	nats "github.com/nats-io/go-nats"
 )
 
+var log = logger.GetLogger("nats-trigger")
+
 // NatsTriggerFactory My Trigger factory
-type NatsTriggerFactory struct{
+type NatsTriggerFactory struct {
 	metadata *trigger.Metadata
 }
 
 //NewFactory create a new Trigger factory
 func NewFactory(md *trigger.Metadata) trigger.Factory {
-	return &NatsTriggerFactory{metadata:md}
+	return &NatsTriggerFactory{metadata: md}
 }
 
 //New Creates a new trigger instance for a given id
 func (t *NatsTriggerFactory) New(config *trigger.Config) trigger.Trigger {
-	return &NatsTrigger{metadata: t.metadata, config:config}
+	return &NatsTrigger{metadata: t.metadata, config: config}
 }
 
 // NatsTrigger is a stub for your Trigger implementation
@@ -48,6 +51,15 @@ func (t *NatsTrigger) Metadata() *trigger.Metadata {
 	return t.metadata
 }
 
+const jsonObjectMarker = byte('{')
+const jsonArrayMarker = byte('[')
+const jsonStringMarker = byte('"')
+
+const IS_UNKNOWN = 0
+const IS_OBJECT = 1
+const IS_ARRAY = 2
+const IS_STRING = 3
+
 // Start implements trigger.Trigger.Start
 func (t *NatsTrigger) Start() error {
 	nc, _ := nats.Connect(nats.DefaultURL)
@@ -55,16 +67,57 @@ func (t *NatsTrigger) Start() error {
 	for _, handler := range t.handlers {
 		//subject := handler.GetStringSetting("subject")
 
-		nc.Subscribe("*", func(m *nats.Msg) {
-		//nc.Subscribe(subject, func(m *nats.Msg) {
-			fmt.Printf("Received a message: %s\n", string(m.Data))
-			var f map[string]interface{}
-			json.Unmarshal(m.Data, &f)
-			handler.Handle(context.Background(), f)
+		nc.Subscribe(">", func(m *nats.Msg) {
+
+			jsonType := getJsonType(m.Data)
+			switch jsonType {
+			case IS_OBJECT:
+				var content map[string]interface{}
+				json.Unmarshal(m.Data, &content)
+				log.Debugf("IS OBJECT %v", content)
+				handler.Handle(context.Background(), map[string]interface{}{
+					"subject": m.Subject,
+					"content": content,
+				})
+				break
+			case IS_ARRAY:
+				var content map[string]([]interface{})
+				json.Unmarshal(m.Data, &content)
+				log.Debugf("IS ARRAY %v", content)
+				handler.Handle(context.Background(), map[string]interface{}{
+					"subject": m.Subject,
+					"content": content,
+				})
+				break
+			case IS_STRING:
+				var content string
+				json.Unmarshal(m.Data, &content)
+				handler.Handle(context.Background(), map[string]interface{}{
+					"subject": m.Subject,
+					"content": content,
+				})
+				break
+			case IS_UNKNOWN:
+				log.Errorf("Could not extract type from message %v", m.Data)
+			}
 		})
 	}
 	// start the trigger
 	return nil
+}
+
+func getJsonType(data []byte) int {
+	for _, element := range data {
+		switch element {
+		case jsonObjectMarker:
+			return IS_OBJECT
+		case jsonArrayMarker:
+			return IS_ARRAY
+		case jsonStringMarker:
+			return IS_STRING
+		}
+	}
+	return IS_UNKNOWN
 }
 
 // Stop implements trigger.Trigger.Start
